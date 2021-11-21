3398e236f703818c160dbdc211ac15e49fdec2a7
suchme <qeououo@gmail.com>
2021-11-21 17:42:06 +0900

    wip

--- src/main.js
+++ src/main.js
@@ -719,7 +719,7 @@ class Main {
 
 
 		var dt=new Date(DATA.date);
-		values.version="code2021/10/16\n"
+		values.version="code2021/11/21\n"
 			+ "data"+ dt.getFullYear() +"/"+("0"+(dt.getMonth()+1)).slice(-2)
 			+"/" +("0"+dt.getDate()).slice(-2);
 
