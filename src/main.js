a3127c3878c93e9f612eb4dc46ae72b3e26bebc6
suchme <qeououo@gmail.com>
2021-12-04 22:49:14 +0900

    fix data

--- src/main.js
+++ src/main.js
@@ -719,7 +719,7 @@ class Main {
 
 
 		var dt=new Date(DATA.date);
-		values.version="code2021/12/03\n"
+		values.version="code2021/12/04\n"
 			+ "data"+ dt.getFullYear() +"/"+("0"+(dt.getMonth()+1)).slice(-2)
 			+"/" +("0"+dt.getDate()).slice(-2);
 		globalParam.version=20211130;
